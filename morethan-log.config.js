51a822d1f58b031070a95cdbb0eef16e3f11de80
morethanmin <mini4614@gmail.com>
2022-12-17 00:03:48 +0900

    chore: delete firebase deploy

--- morethan-log.config.js
+++ morethan-log.config.js
@@ -22,7 +22,7 @@ const CONFIG = {
   },
 
   // CONFIG configration
-  link: 'https://morethanmin.web.app',
+  link: 'https://morethanmin.com',
   since: 2022, // If leave this empty, current year will be used.
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   postsPerPage: 10,
